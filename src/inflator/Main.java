3fc67335dd0cb78df4a1675e47d4753c4bb6ef5c
sucoso <65839195+nitsuboy@users.noreply.github.com>
2023-03-03 06:54:57 -0300

    the sky is the limit

--- src/inflator/Main.java
+++ src/inflator/Main.java
@@ -1,44 +1,51 @@
 package inflator;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		if(args.length == 0) {
-			System.exit(1);
-		}
+//		if(args.length == 0) {
+//			System.exit(1);
+//		}
+//		
+//		File originalfile = new File(args[0]);
 		
-		File originalfile = new File(args[0]);
+		String path = "F:\\midia\\Trollface.png";
 		
-//		File originalfile = new File("F:\\papo_de_rato_1.zip");
+		int b = 1024;
+		long v = 1024 * 1;
+		
+		byte[] result = new byte[b * 1024];
+		System.arraycopy("0".getBytes(), 0, result, 100 - "0".length(), "0".length());
 
-		long v = Long.parseLong(args[1]);
-		
-//		long v = 758128745;
-		
-		byte[] bytes = Files.readAllBytes(originalfile.toPath());
+		File file = new File(path);
+	    
+	    try (
+	    	      InputStream inputStream = new FileInputStream(file);
+	    	      BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
 
-		int f = (int) (v - originalfile.length());
-		if (f < 0) {
-			System.out.println("nah, the file is bigger :\\");
-			System.exit(1);
-		}
-		
-		String str = "0";
-		byte[] byteArr = str.getBytes();
-		
-		byte[] finalArray = new byte[bytes.length + f];
-		System.arraycopy(bytes, 0, finalArray, 0, bytes.length);
-		
-		for (int i = 0; i < f; i++) {
-			System.arraycopy(byteArr, 0, finalArray, bytes.length + i, byteArr.length);		
-		}
-
-		Files.write(originalfile.toPath(), finalArray);
+	    	      OutputStream outputStream = new FileOutputStream("result.png");
+	    	  ) {
+	    	    byte[] buffer = new byte[4 * 1024];
+	    	    int read;
+	    	    while ((read = bufferedInputStream.read(buffer, 0, buffer.length)) != -1) {
+	    	    	outputStream.write(buffer, 0, read);
+	    	    }
+	    	    for(long i = 0; i<v;i++ ) {
+	    	    	outputStream.write(result);
+	    	    }
+	    	  }
+	    
+		System.out.println("DONE");	
+		System.exit(0);
 		
 		System.out.println("DONE");	
 		System.exit(0);
